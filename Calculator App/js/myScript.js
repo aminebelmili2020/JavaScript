6cae30cd9a5be87c293b3ea5cde4b1af9f97306a
aminebelmili2020 <aminelearning1@gmail.com>
2022-08-05 08:57:28 -0700

    v1.0.0 isDone ♥

--- Calculator App/js/myScript.js
+++ Calculator App/js/myScript.js
@@ -1,4 +1,30 @@
-let names=[['amine',27,'algeria'], ['ahmed',50,'algeria']]
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
 
-names.pop()
-console.log(names);
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    } else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Empty!";
+      setTimeout(() => (display.innerText = ""), 2000);
+    } else {
+      display.innerText += item.id;
+    }
+  };
+});
+
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
